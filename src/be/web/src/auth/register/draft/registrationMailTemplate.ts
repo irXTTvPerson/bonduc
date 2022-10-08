e1cb508fd23a7c9eb1586c3a559736cc65efb66a
irXTTvPerson <irXTTvMail@gmail.com>
2022-10-08 16:44:19 +0900

    [#2/fix/be/web] 本登録アドレスのパスが間違ってemail配信される問題を修正

--- src/be/web/src/auth/register/draft/registrationMailTemplate.ts
+++ src/be/web/src/auth/register/draft/registrationMailTemplate.ts
@@ -3,5 +3,5 @@ import { Config } from "../../../config";
 export const subject = "bonduc 正規登録のお願い";
 
 export const body = (token: string) => {
-  return `${Config.feEndpoint}/register?token=${token}<br><br>にアクセスして登録を完了してください`;
+  return `${Config.feEndpoint}/auth/register?token=${token}<br><br>にアクセスして登録を完了してください`;
 };
