66b69eb599db658660fe1ff1b0b546797c8cf54b
irXTTvPerson <irXTTvMail@gmail.com>
2022-10-19 19:16:09 +0900

    [#10/dev/be/web] graphql経由でpodの取得と更新を実装

--- src/be/web/src/app.module.ts
+++ src/be/web/src/app.module.ts
@@ -1,8 +1,23 @@
 import { Module } from "@nestjs/common";
 import { AuthModule } from "./auth/auth.module";
 import { ConfigModule } from "@nestjs/config";
+import { join } from "path";
+import { GraphQLModule } from "@nestjs/graphql";
+import { ApolloDriver, ApolloDriverConfig } from "@nestjs/apollo";
+import { Config } from "./config";
+import { PodResolver } from "./pod/pod.resolver";
 
 @Module({
-  imports: [AuthModule, ConfigModule.forRoot()]
+  imports: [
+    AuthModule,
+    ConfigModule.forRoot(),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      debug: Config.isLocalEnv ? true : false,
+      playground: Config.isLocalEnv ? true : false,
+      autoSchemaFile: join(process.cwd(), "src/schema.gql")
+    })
+  ],
+  providers: [PodResolver]
 })
 export class AppModule {}
