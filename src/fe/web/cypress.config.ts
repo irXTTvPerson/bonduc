0d4de8ba68ac1d1a710eaf5e3e2340b7ad84cad2
irXTTvPerson <irXTTvMail@gmail.com>
2022-10-24 18:01:36 +0900

    [#10/test/fe/web] アカウント登録のテストを無効化

--- src/fe/web/cypress.config.ts
+++ src/fe/web/cypress.config.ts
@@ -1,10 +1,10 @@
-import { defineConfig } from "cypress";
+import { defineConfig } from "cypress"
 
 export default defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
-    },
+    }
   },
   env: {
     test_target_url: "http://localhost:8080",
@@ -12,5 +12,6 @@ export default defineConfig({
     do_test_draft_account_registration: false,
     do_test_account_registration: false, // create DraftAccount before enable this
     do_test_account_registration_token: "", // set DraftAccount token
+    do_test_login: false
   }
-});
+})
